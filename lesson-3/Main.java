539b6d99cb815d9040c4a612ece01be9755946e1
Dekrate <51668658+Dekrate@users.noreply.github.com>
2022-11-07 17:54:03 +0100

    Update Main.java

--- lesson-3/Main.java
+++ lesson-3/Main.java
@@ -1,5 +1,13 @@
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
-
+        Book book = new Book(new ArrayList<>() {{
+            add(new Author("Jan", "Kowalski", "Polska"));
+        }}, new ArrayList<>() {{
+            add(new Title("Przygody Pana Muchomora", "Polski"));
+            add(new Title("Tales of Mr Toadstool", "English"));
+        }}, new ISBN("12345678901"), new DateOfProduction(2021, 12, 8), false);
+        book.autograph();
     }
 }
