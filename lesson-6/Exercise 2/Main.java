625d3008f19dafeca49f979e91d869e3ba0f4deb
Dekrate <51668658+Dekrate@users.noreply.github.com>
2022-11-28 17:30:28 +0100

    Create Main.java

---
+++ lesson-6/Exercise 2/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        Operations operations = new Operations(3, 6);
+        System.out.println(operations.max());
+        System.out.println(operations.min());
+        System.out.println(operations.avg());
+    }
+}
