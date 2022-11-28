b5bb3b452b0e6e6240a3903ea8182d9f7016da79
Dekrate <51668658+Dekrate@users.noreply.github.com>
2022-11-28 17:29:20 +0100

    Create Logarithm.java

---
+++ lesson-6/Exercise 1/Logarithm.java
@@ -0,0 +1,22 @@
+
+
+// Before refactor
+public class Logarithm implements Numbers {
+
+    private final double base;
+    private final double argument;
+
+    @Override
+    public double doubleValue() throws ArithmeticException {
+        if (base > 0 && base != 1 && argument > 0) {
+            return Math.log(this.argument) / Math.log(this.base);
+        } else {
+            throw new ArithmeticException("Invalid argument - check if base > 0 && base != 1 and argument > 0");
+        }
+    }
+
+    public Logarithm(double inputBase, double inputArgument) {
+        this.base = inputBase;
+        this.argument = inputArgument;
+    }
+}
