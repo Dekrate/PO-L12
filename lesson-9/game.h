7668bbe61fe19ee46a53bdd7318dfb661e1ae451
Dekrate <51668658+Dekrate@users.noreply.github.com>
2023-01-07 12:34:24 +0100

    Create game.h

---
+++ lesson-9/game.h
@@ -0,0 +1,17 @@
+//
+// Created by Acer on 07.01.2023.
+//
+
+#ifndef OOP___FAKE_GAME_H
+#define OOP___FAKE_GAME_H
+
+
+#include <string>
+
+class Game {
+public:
+    virtual std::string result() = 0;
+};
+
+
+#endif //OOP___FAKE_GAME_H
